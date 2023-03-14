1f48aa31e4ecf2a557a23827b40098e5ebbfa92f
m27arora <m27arora@uwaterloo.ca>
2023-03-14 18:53:19 -0400

    reports fragment changes

--- app/src/main/java/com/example/appause/ui/reports/RecyclerAdapter.kt
+++ app/src/main/java/com/example/appause/ui/reports/RecyclerAdapter.kt
@@ -1,20 +1,58 @@
 package com.example.appause.ui.reports
 
+import android.annotation.SuppressLint
+import android.os.Build
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
-import com.example.appause.R
+import com.example.appause.*
 
-class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(mainActivity: MainActivity) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    //
+    private val mainActivity: MainActivity = mainActivity
+    private var context = mainActivity.applicationContext
+    private var appTimer = AppTimer(context)
 
-    private val description = arrayOf("Total Screen Time", "Social", "Productivity", "Video", "Entertainment", "Movies")
+    private val description = arrayOf("Social", "Productivity", "Video", "Entertainment", "Movies")
 
-    private val usageTime = arrayOf("1 / 4 h", "1 / 0.5 h", "0 / 3 h", "0.5 / 0.5 h", "4 / 2 h", " 5 / 2 h")
+    private val totalTime = arrayOf(10, 5, 4, 1, 5)
 
-    private val time = arrayOf(10, 5, 4, 1, 5, 9)
+    // TODO use these in future
+
+    // This list contains goalCategories and goalTime
+    private var goals : List<Goal> = emptyList()
+    // this variable represents total screen time
+    private var totalTimeCurr: Long = 100
+    // this represents usage for ith category
+    private var goalTimeUsedCurr : List<Long> = listOf(1, 0, 0.5, 4, 5) as List<Long>
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
+    fun updateData() {
+        // this is getting actual data
+        // TODO fix the error on this line
+        // appTimer.getCurrentUsage()
+        var totalScreenTime = GoalTracker.totalTimeCurr
+
+        // Dummy data
+        val goal = Goal("", 0, listOf(), listOf())
+        goal.goalName = "Total Screen Time"
+        goal.goalTime = totalTimeCurr
+        val newGoalList : MutableList<Goal> = mutableListOf(goal)
+        for (i in description.indices) {
+            val goal = Goal("", 0, listOf(), listOf())
+            goal.goalName = description[i]
+            goal.goalTime = totalTime[i].toLong()
+            newGoalList.add(goal)
+        }
+
+        goals = newGoalList
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -24,23 +62,39 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
         return ViewHolder(v)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
 
-        val image : Int = if (time[i] > 5) {
+        val image = getViewImage(i)
+
+        holder.hourglass.setImageResource(image)
+        holder.description.text = goals[i].goalName
+
+        val usage : String = if (goals[i].goalName == "Total Screen Time") {
+            goals[i].goalTime.toString() + " h"
+        } else {
+            goalTimeUsedCurr[i-1].toString() + " / " + goals[i].goalTime.toString() + " h"
+        }
+        holder.usageTime.text = usage
+    }
+
+    override fun getItemCount(): Int {
+        return goals.size
+    }
+
+    private fun getViewImage(position: Int): Int {
+
+        // TODO: implement logic that if 50% used, then glass half full etc then implement actual data logic
+        val time = goals[position].goalTime
+
+        val image : Int = if (time > 5) {
             R.drawable.ic_hourglass_bottom
-        } else if (time[i] == 5) {
+        } else if (time == 5L) {
             R.drawable.ic_hourglass_full
         } else {
             R.drawable.ic_hourglass_top
         }
-
-        holder.hourglass.setImageResource(image)
-        holder.description.text = description[i]
-        holder.usageTime.text = usageTime[i]
-    }
-
-    override fun getItemCount(): Int {
-        return description.size
+        return image
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
