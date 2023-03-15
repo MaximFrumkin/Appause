ffb6e639bbda14cd2f8491900e05700b9de17e65
Nicole Qian <nicoleqian@Nicoles-MacBook-Pro-2.local>
2023-03-14 22:54:12 -0400

    friends ui incomplete

---
+++ app/src/main/java/com/example/appause/ui/friends/FriendsFragment.kt
@@ -0,0 +1,36 @@
+package com.example.appause.ui.friends
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appause.R
+import com.example.appause.databinding.FragmentReportsBinding
+import com.example.appause.ui.reports.RecyclerAdapter
+
+class FriendsFragment : Fragment() {
+    private var _binding: FragmentReportsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.friends, container, false)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.friends_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = FriendsRecyclerAdapter()
+
+        return view
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
