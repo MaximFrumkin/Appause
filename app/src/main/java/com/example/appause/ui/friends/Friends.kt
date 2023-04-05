93aa00802199c1818060ad23648ce68534113b18
Nicole Qian <nicoleqian@v1040-wn-rt-a-75-64.campus-dynamic.uwaterloo.ca>
2023-04-04 20:25:58 -0400

    total screen time image updated

--- app/src/main/java/com/example/appause/ui/friends/Friends.kt
+++ app/src/main/java/com/example/appause/ui/friends/Friends.kt
@@ -7,12 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appause.R
-
 
 class Friends : Fragment() {
 
